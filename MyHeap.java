787389062268dfcd83ffdd7f4a02a14cb2d5f841
Sharon Zou <szou00@stuy.edu>
2019-04-29 00:32:11 -0400

    wrote pushdown

---
+++ MyHeap.java
@@ -0,0 +1,57 @@
+public class MyHeap {
+
+  /*
+  - size  is the number of elements in the data array.
+     - push the element at index i downward into the correct position. This will swap with the larger of the child nodes provided thatchild is larger. This stops when a leaf is reached, or neither child is larger. [ should be O(logn) ]
+     - precondition: index is between 0 and size-1 inclusive
+     - precondition: size is between 0 and data.length-1 inclusive.
+  */
+  private static void pushDown(int[]data,int size,int index) {
+    boolean done = false;
+    while (!done) {
+      int leftchild = index * 2 + 1; //index of left child
+      int rightchild = leftchild + 1; //index of right child
+
+      if (rightchild == size) { //if there is exactly one child
+        if (data[leftchild] > data[index]) {
+          int larger = data[leftchild];
+          data[leftchild] = data[index];
+          data[index] = larger;
+        }
+        else {
+          done = true;
+        }
+      }
+
+      else if (rightchild < size) { //there are two children
+
+        if (data[leftchild] > data[rightchild]) {
+          if (data[leftchild] > data[index]) {
+            int larger = data[leftchild];
+            data[leftchild] = data[index];
+            data[index] = larger;
+          }
+          else {
+            done = true;
+          }
+        }
+        else {
+          if (data[rightchild] > data[index]) {
+            int larger = data[rightchild];
+            data[rightchild] = data[index];
+            data[index] = larger;
+          }
+          else {
+            done = true;
+          }
+        }
+      }
+      else {
+        done = true;
+      }
+    }
+  }
+
+
+
+}
